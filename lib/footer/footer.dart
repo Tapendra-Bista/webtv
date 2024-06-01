709719d2e72c61cee79c14cb2c4a8cc598442cda
tapendrabistao1 <119024299+Tapendra-Bista@users.noreply.github.com>
2024-06-01 21:33:53 +0545

    Update footer.dart

--- lib/footer/footer.dart
+++ lib/footer/footer.dart
@@ -28,7 +28,7 @@ class Footer extends StatelessWidget {
                 children: [
               TextSpan(
                   text:
-                      "\n\nIt is our policy to respond to clear notices of alleged copyright infringement. If you are a copyright owner or an agent thereof, and you believe that any content hosted on our web site (mobileTv) infringes your copyrights, then you may submit a notification for us in email (tapendrabista01@gmail.com)",
+                      "\nIt is our policy to respond to clear notices of alleged copyright infringement. If you are a copyright owner or an agent thereof, and you believe that any content hosted on our web site (mobileTv) infringes your copyrights, then you may submit a notification for us in email (tapendrabista01@gmail.com)",
                   style: mytextStyle!.copyWith(fontSize:15, color: Colors.black))
             ])),
       ),
